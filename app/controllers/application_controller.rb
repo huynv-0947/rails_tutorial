146f2afb55800ad014f65a2031ad0452a648d6cb
Nguyen Huy <nguyen.van.huy@sun-asterisk.com>
2019-09-03 14:34:57 +0700

    chapter 3: static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "hello, world!"
+  before_action :setlocale
+
+  private
+
+  def setlocale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  private 
-
-    def setlocale
-      I18n.locale = params[:locale] || I18n.default_locale
-    end
-
-    def default_url_options
-      {locale: I18n.locale}
-    end
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
