37429e8696acffcfebae62d311df2fb0a2c86d6a
Nguyen Huy <nguyen.van.huy@sun-asterisk.com>
2019-09-09 10:36:54 +0700

    chapter 7: signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
 
     title + " | " + sub_title
   end
+
+  def gravatar_for user
+    gravatar_id = Digest::MD5.hexdigest user.email.downcase
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
